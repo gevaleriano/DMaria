154f34f0169c4b307f58f75ead60fabd5634960e
German Gonzalez Valeriano <valeriano@Germans-MacBook-Pro.local>
2023-12-07 22:13:59 -0600

    creacion de tablas

--- resources/views/formulario-cliente.blade.php
+++ resources/views/formulario-cliente.blade.php
@@ -18,6 +18,7 @@
 </figure>
 
 <form>
+<!-- Informacion del cliente -->
 <div class="card">
   <div class="card-header">
     Informacion del cliente
@@ -76,6 +77,7 @@
   </div>
 </div>
 <div class="clearfix">&nbsp;</div>
+<!-- Informacion General del cliente -->
 <div class="card">
   <div class="card-header">
     Informacion General del cliente
@@ -169,6 +171,7 @@
   </div>
 </div> 
 <div class="clearfix">&nbsp;</div>
+<!-- Datos del representante o apoderado -->
 <div class="card">
   <div class="card-header">
     Datos del representante o apoderado
@@ -242,6 +245,7 @@
   </div>
 </div> 
 <div class="clearfix">&nbsp;</div>
+<!-- Area de compras (encargado de compras, duenos del negocio o gerente de compras) -->
 <div class="card">
   <div class="card-header">
     Area de compras (encargado de compras, duenos del negocio o gerente de compras)
@@ -270,6 +274,7 @@
   </div>
 </div>
 <div class="clearfix">&nbsp;</div>
+<!-- Identificacion de persona expuesta politicamente -->
 <div class="card">
   <div class="card-header">
     Identificacion de persona expuesta politicamente
@@ -334,6 +339,7 @@
   </div>
 </div>
 <div class="clearfix">&nbsp;</div>
+<!-- Identificacion general del negocio -->
 <div class="card">
   <div class="card-header">
     Identificacion general del negocio
@@ -415,6 +421,230 @@
         <div class="col-3" >
             <label for="pasivos" class="form-label">Total Pasivos</label>
             <input type="text" class="form-control form-control-sm" id="pasivos">
+        </div>
+    </div>
+  </div>
+</div>
+<div class="clearfix">&nbsp;</div>
+<!-- Principales Proveedores -->
+<div class="card">
+  <div class="card-header">
+    Principales Proveedores
+  </div>
+  <div class="card-body">
+    <div class="row">
+        <div class="col-4" >
+            <label for="proveedor1" class="form-label">Nombre Proveedor</label>
+            <input type="text" class="form-control form-control-sm" id="proveedor1">
+        </div>
+        <div class="col-5" >
+            <label for="direccion1" class="form-label">Direccion</label>
+            <input type="text" class="form-control form-control-sm" id="direccion1">
+        </div>
+        <div class="col-3" >
+            <label for="telefono1" class="form-label">Telefono</label>
+            <input type="text" class="form-control form-control-sm" id="telefono1">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4" >
+            <label for="proveedor2" class="form-label">Nombre Proveedor</label>
+            <input type="text" class="form-control form-control-sm" id="proveedor2">
+        </div>
+        <div class="col-5" >
+            <label for="direccion2" class="form-label">Direccion</label>
+            <input type="text" class="form-control form-control-sm" id="direccion2">
+        </div>
+        <div class="col-3" >
+            <label for="telefono2" class="form-label">Telefono</label>
+            <input type="text" class="form-control form-control-sm" id="telefono2">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4" >
+            <label for="proveedor3" class="form-label">Nombre Proveedor</label>
+            <input type="text" class="form-control form-control-sm" id="proveedor3">
+        </div>
+        <div class="col-5" >
+            <label for="direccion3" class="form-label">Direccion</label>
+            <input type="text" class="form-control form-control-sm" id="direccion3">
+        </div>
+        <div class="col-3" >
+            <label for="telefono3" class="form-label">Telefono</label>
+            <input type="text" class="form-control form-control-sm" id="telefono3">
+        </div>
+    </div>
+  </div>
+</div>
+<div class="clearfix">&nbsp;</div>
+<!-- Principales clientes -->
+<div class="card">
+  <div class="card-header">
+    Principales clientes
+  </div>
+  <div class="card-body">
+    <div class="row">
+        <div class="col-4" >
+            <label for="cliente1" class="form-label">Nombre Cliente</label>
+            <input type="text" class="form-control form-control-sm" id="cliente1">
+        </div>
+        <div class="col-5" >
+            <label for="direccion1" class="form-label">Direccion</label>
+            <input type="text" class="form-control form-control-sm" id="direccion1">
+        </div>
+        <div class="col-3" >
+            <label for="telefono1" class="form-label">Telefono</label>
+            <input type="text" class="form-control form-control-sm" id="telefono1">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4" >
+            <input type="text" class="form-control form-control-sm" id="cliente2">
+        </div>
+        <div class="col-5" >
+            <input type="text" class="form-control form-control-sm" id="direccion2">
+        </div>
+        <div class="col-3" >
+            <input type="text" class="form-control form-control-sm" id="telefono2">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4" >
+            <input type="text" class="form-control form-control-sm" id="cliente3">
+        </div>
+        <div class="col-5" >
+            <input type="text" class="form-control form-control-sm" id="direccion3">
+        </div>
+        <div class="col-3" >
+            <input type="text" class="form-control form-control-sm" id="telefono3">
+        </div>
+    </div>
+  </div>
+</div>
+<div class="clearfix">&nbsp;</div>
+<!-- Composicion Accionaria -->
+<div class="card">
+  <div class="card-header">
+    Composicion Accionaria
+  </div>
+  <div class="card-body">
+    <div class="row">
+        <div class="col-4" >
+            <label for="accionista1" class="form-label">Nombre completo del funcionario</label>
+            <input type="text" class="form-control form-control-sm" id="accionista1">
+        </div>
+        <div class="col-2" >
+            <label for="participacion1" class="form-label">% de participacion</label>
+            <input type="number" min=0 max=100 class="form-control form-control-sm" id="participacion1">
+        </div>
+        <div class="col-3" >
+            <label for="tipo-doc1" class="form-label">Tipo de documento</label>
+            <input type="text" class="form-control form-control-sm" id="tipo-doc1">
+        </div>
+        <div class="col-3" >
+            <label for="documento1" class="form-label">Numero de documento</label>
+            <input type="text" class="form-control form-control-sm" id="documento1">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4" >
+            <input type="text" class="form-control form-control-sm" id="accionista2">
+        </div>
+        <div class="col-2" >
+            <input type="number" min=0 max=100 class="form-control form-control-sm" id="participacion2">
+        </div>
+        <div class="col-3" >
+            <input type="text" class="form-control form-control-sm" id="tipo-doc2">
+        </div>
+        <div class="col-3" >
+            <input type="text" class="form-control form-control-sm" id="documento2">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4" >
+            <input type="text" class="form-control form-control-sm" id="accionista3">
+        </div>
+        <div class="col-2" >
+            <input type="number" min=0 max=100 class="form-control form-control-sm" id="participacion3">
+        </div>
+        <div class="col-3" >
+            <input type="text" class="form-control form-control-sm" id="tipo-doc3">
+        </div>
+        <div class="col-3" >
+            <input type="text" class="form-control form-control-sm" id="documento3">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4" >
+            <input type="text" class="form-control form-control-sm" id="accionista4">
+        </div>
+        <div class="col-2" >
+            <input type="number" min=0 max=100 class="form-control form-control-sm" id="participacion4">
+        </div>
+        <div class="col-3" >
+            <input type="text" class="form-control form-control-sm" id="tipo-doc4">
+        </div>
+        <div class="col-3" >
+            <input type="text" class="form-control form-control-sm" id="documento4">
+        </div>
+    </div>
+  </div>
+</div>
+<div class="clearfix">&nbsp;</div>
+<!-- Identificacion de la persona que realiza el pago -->
+<div class="card">
+  <div class="card-header">
+  Identificacion de la persona que realiza el pago
+  </div>
+  <div class="card-body">
+    <div class="row">
+        <div class="col-4" >
+            <label for="encargado-pago" class="form-label">Nombre completo del funcionario</label>
+            <input type="text" class="form-control form-control-sm" id="encargado-pago">
+        </div>
+        <div class="col-2" >
+            <label for="cargo" class="form-label">cargo</label>
+            <input type="text" class="form-control form-control-sm" id="cargo">
+        </div>
+        <div class="col-3" >
+            <label for="tipo-doc-pago" class="form-label">Tipo de documento</label>
+            <input type="text" class="form-control form-control-sm" id="tipo-doc1">
+        </div>
+        <div class="col-3" >
+            <label for="documento-encargado" class="form-label">Numero de documento</label>
+            <input type="text" class="form-control form-control-sm" id="documento-encargado">
+        </div>
+    </div>
+  </div>
+</div>
+<div class="clearfix">&nbsp;</div>
+<!-- Declaracion jurada -->
+<div class="card">
+  <div class="card-header">
+  Declaracion jurada
+  </div>
+  <div class="card-body">
+    <div class="row">
+        <div class="col-12" >
+           <p class="text-break">
+           DECLARO BAJO JURAMENTO que los bienes y servicios registrados en este documento, así como lo que resultare de la aplicación de la presente solicitud tienen un origen y destino lícito, y que
+no provienen de actividades de Lavado de Dinero y de Activos y Financiamiento al Terrorismo, o contempladas en el Art. 6 de la Ley Contra el Lavado de Dinero y Activos u otros tipificados en
+el Art. 29 de la Ley Especial Contra Actos de Terrorismo.
+Además, en el caso de representar a persona jurídica declaro que los miembros de la Junta Directiva, accionistas, representantes legales (inclusive mi persona) y administradores, no estamos
+incluidos en los listados de la Oficina del Control de Activos Extranjeros (OFAC), lista INTERPOL, o en otro listado de similar naturaleza, o en algún listado de carácter nacional y/o internacional
+en la que se publiquen nombres de personas o entidades a quienes se les haya iniciado proceso judicial, actuación administrativa o que hayan sido sancionadas y/o condenadas por las
+autoridades nacionales e internacionales, de manera directa o indirecta, con actividades ilegales, tales como lavado de dinero, narcotráfico, terrorismo o su financiamiento, tráfico de
+estupefacientes, secuestro, extorsiones, trata de personas u otras enunciados en el artículo 6 de la Ley Contra el Lavado de Dinero y Activos.
+Así mismo declaro que la información anterior es fidedigna y que me ha sido solicitado por , S.A de C.V., en cumplimiento a lo dispuesto en los artículos 9B y 10 de la
+Ley Contra el Lavado de Dinero y Activos y de los artículos del 12 al 15 del Instructivo de la Unidad de Investigación Financiera para la Prevención de Lavado de Dinero y Activos. De la misma
+forma autorizo a , S.A de C.V. a que realice las investigaciones que estime conveniente para verificarlo, comprometiéndome también a actualizar la documentación cuando me sea
+requerido por la empresa correspondiente, comunicando de manera inmediata cualquier cambio en la misma. Adicionalmente autorizo a , S.A de C.V., para que confirme la
+veracidad de la información a través de los medios que considere conveniente.
+Además, eximo a los miembros de Junta Directiva, accionistas, socios, gerentes, jefes, supervisores, otro personal administrativo, de servicio, operativos y subcontratados relacionados con la
+empresa de toda responsabilidad, inclusive a terceros, si esta declaración fuese falsa o errónea. En virtud de lo autorizado, renuncio a instaurar por este motivo cualquier tipo de acción civil,
+penal o administrativa en contra de , S.A de C.V., de sus representantes legales, funcionarios, empleados y terceros relacionados en la actividad comercia
+Como constancia de haber leído, comprendido y aceptado lo anterior, declaro que la información que he suministrado es exacta en todas sus partes y firmo el presente documento.
+           </p> 
         </div>
     </div>
   </div>
